6dad601c25593612a0f1b944cdc41284153501a0
Alp Bora Eris <160424632+alpboraeris@users.noreply.github.com>
2025-11-09 21:57:11 +0100

    Update app.py

--- app.py
+++ app.py
@@ -10,8 +10,8 @@ le_smoker = joblib.load("label_encoder_smoker.pkl")
 model = joblib.load("best_model.pkl")
 
 st.set_page_config(page_title = "Insurance Claim Predictor", layout = "centered")
-st.title("Health Insurance Payment Prediction App")
-st.write("Enter the details below to estimate your insurance payment amount")
+st.title("Health Insurance Claim Prediction App")
+st.write("Enter the details below to estimate your insurance claim amount")
 
 with st.form("input_form"):
     col1, col2 = st.columns(2)
@@ -25,7 +25,7 @@ with st.form("input_form"):
         diabetic = st.selectbox("Diabetic", options= le_diabetic.classes_)
         smoker = st.selectbox("Smoker", options= le_smoker.classes_)
 
-    submitted = st.form_submit_button("Predict Payment")
+    submitted = st.form_submit_button("Predict Claim")
 
 if submitted:
     input_data = pd.DataFrame({
@@ -47,5 +47,6 @@ if submitted:
 
     prediction = model.predict(input_data)[0]
 
-    st.success(f"**Estimated Insurrance Payment Amount:** ${prediction:,.2f}")
+    st.success(f"**Estimated Insurrance Claim Amount:** ${prediction:,.2f}")
+
     
